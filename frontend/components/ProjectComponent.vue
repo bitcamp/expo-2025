64428695116aeeae7b567c4c9492ec64a699657e
Sai Pranav Theerthala <sptheerthala@gmail.com>
2024-03-26 22:22:15 -0400

    fixed routing to show correct pages

---
+++ frontend/components/ProjectComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="entire-container">
+        <div class="top-row">
+            <div class="table-header">Table</div>
+            <div class="project-header">Project</div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss"></style>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="app-container">
+        <ProjectComponent />
+    </div>
+</template>
+
+<script lang="ts">
+// import ProjectComponent from './components/ProjectComponent.vue';
+export default {
+    name: 'ProjectComponent',
+};
+
+</script>
+
+<style scoped lang="scss"></style>
