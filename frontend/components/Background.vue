a5343309a036dc37ee9c58fd9500ef7da296b16e
Mohe Edeen Abu Maizer <86363939+MoheEdeen@users.noreply.github.com>
2024-03-28 22:56:37 -0400

    fixed variable prop

---
+++ frontend/components/Background.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="hero-wrapper">      
+        <div class="hero">
+            <img class="foilage-large" src="~/assets/images/background/heroFoilage.svg"
+        />
+      </div>
+    </div>
+</template>  
+  <script setup lang="ts">
+  
+  </script>
